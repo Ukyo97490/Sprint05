2b1a4b56d9626a975be5d1490f99b2053d0ddcb3
Ukyo97490 <80383797+Ukyo97490@users.noreply.github.com>
2022-03-11 13:32:23 +0400

    Rattrapage d'erreur ?

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
-async function render() {
-  let rep = await fetch("https://restcountries.com/v3.1/region/europe"); //récupère le contenu d'un lien
-
-  let data = await rep.json(); //transforme le contenu du lien en json
-  let result = "";
-
-  console.log(rep);
-  if ((rep.ok = true)) {
-    for (country in data) {
-      result += `<li> ${data[country].name.official} </li>`;
-    }
-  } else {
-    rep.catch((error) => alert("Erreur : " + error));
+let data = await rep.json();
+let result = "";
+console.log(rep);
+if ((rep.ok = true)) {
+  for (country in data) {
+    result += `<li> ${data[country].name.official} </li>`;
   }
-
-  document.getElementById("list").innerHTML = result; //rajouter l'élément dans une liste
+} else {
+  rep.catch((error) => alert("Erreur : " + error));
 }
-render();
+document.getElementById("list").innerHTML = result;
 
+render();
 const reloadUsingLocationHash = () => {
   window.location.hash = "reload";
 };
